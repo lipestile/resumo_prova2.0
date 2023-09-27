5527cac426dd72dbbeb9833d043df96117ff8fe4
lipestile <felipebalbino53@gmail.com>
2023-09-26 22:38:52 -0300

    felipe

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import Router from './src/routes/Router';
-
+// felipe carvalho balbino da silva
 export default function App() {
   return (
    <PaperProvider>
